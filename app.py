891c369c1f2b4841e0fc111776194118b9cfd18d
tejveersinghgoraya <56366718+tejveersinghgoraya@users.noreply.github.com>
2021-09-23 15:23:00 +0100

    html

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+
+
+app = Flask(__name__) #referencing the file
+
+
+
+
+
+
+
+@app.route('/')
+def index():
+    return "test"
+
+if __name__ == "__main__":
+    app.run(debug=True)
